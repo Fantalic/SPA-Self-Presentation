4acdadbad01b1ec7fc9bf3c7fac01c93743a3220
unknown <wilkeoncken@protonmail.com>
2023-05-19 12:35:11 +0200

    added AnimatedText

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
+<script>
+import AnimatedText from "@/components/AnimatedText.vue";
+
+export default {
+  components: {
+    AnimatedText,
+  },
+  data() {
+    return {
+      options: {
+        licenseKey: 'YOUR_KEY_HEERE',
+        menu: '#menu',
+        anchors: ['page1', 'page2', 'page3'],
+        sectionsColor: ['#41b883', '#ff5f45', '#0798ec'],
+      },
+    }
+  }
+}
+</script>
 <template>
+
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <full-page ref="fullpage" :options="options" id="fullpage">
+      <div class="section">
+        <AnimatedText></AnimatedText> 
+      </div>
+      <div class="section">Second section ...</div>
+    </full-page>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default {
-    target: 'static'
+    target: 'static',
+    modules: [
+        '@fullpage/nuxt-fullpage'
+    ]
 }
